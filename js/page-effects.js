730ff4f26d91af2c02ee6986c00e1c29b3286514
Vinícius Watanabe <vinicius_watanabe@hotmail.com>
2016-02-13 14:27:00 -0200

    botao subir ok

--- js/page-effects.js
+++ js/page-effects.js
@@ -3,6 +3,20 @@ $(document).ready(function(){
 	$('.link-thumb').mouseenter(projectLinkEnter);
 	$('.link-thumb').mouseleave(projectLinkLeave);
 
+	//SCROLL TOP
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+
 	function projectLinkEnter(){
 		$(this).find('.linha-titulo').hide();
 		$(this).find('.linha-titulo-hidden').slideDown(300);

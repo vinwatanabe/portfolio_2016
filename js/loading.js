b77b55d2529ea1e90f6c230da663e58c8904a98a
Vinícius Watanabe <vinicius_watanabe@hotmail.com>
2016-02-13 18:39:37 -0200

    interacoes e animacoes de carregamento ok

--- js/loading.js
+++ js/loading.js
@@ -4,7 +4,19 @@ $(document).ready(function(){
 	$(window).load(function(){
 		$('.load-container').slideUp(function(){
 			$('.load-container').hide();
-			$('#container').fadeIn(2000);
+			$('#container').fadeIn(500);
+		});
+	});
+
+	$('a').click(function(e){
+		var url = $(this).attr('href');
+		e.preventDefault();
+
+		$('#container').fadeOut(500, function(){
+			$('#container').hide();
+			$('.load-container').slideDown(function(){
+				window.open(url, "_self");
+			});
 		});
 	});
 });
